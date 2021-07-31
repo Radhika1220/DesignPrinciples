3f94ff0b1406b9e73fe7c3eca7b90481b7397232
Radhika <radhika.shankar1220@gmail.com>
2021-07-31 11:18:40 +0530

    [Radhika] Single Responsibility Principle

---
+++ DesignPrinciples/Single Responsibilty Principle/ScheduleTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPrinciples
+{
+    //Declaring getter and setter methods
+    public class ScheduleTask
+    {
+        public int TaskId { get; set; }
+        public string Content { get; set; }
+        public DateTime ExecuteOn { get; set; }
+    }
+}
+
---
+++ DesignPrinciples/Single Responsibilty Principle/Scheduler.cs
@@ -0,0 +1,32 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPrinciples
+{
+    public class Scheduler : IEntryManager<ScheduleTask>
+    {
+        //Declaring a list 
+        private  List<ScheduleTask> scheduleTasks;
+        //Constructor
+        public Scheduler()
+        {
+            scheduleTasks = new List<ScheduleTask>();
+        }
+        //ADDENTRY--->To add te data in list
+        public new void AddEntry(ScheduleTask entry)
+        {
+            scheduleTasks.Add(entry);
+        }
+        //REMOVEENTRY---->Remove the index using lambda expression
+        public new void RemoveEntryAt(int index) =>
+            scheduleTasks.RemoveAt(index);
+
+        public override string ToString() =>
+       string.Join(Environment.NewLine, scheduleTasks.Select(x => $"Task with id: {x.TaskId} with content: {x.Content} is going to be executed on: {x.ExecuteOn}"));
+    }
+}
+
+
